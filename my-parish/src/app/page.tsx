4f4282acceb311b5ee14818f4320d63e3ebdfbef
Jakub Kliś <jakubklis2201@gmail.com>
2025-01-24 20:49:45 +0100

    feat: Initialize My Parish app with Next.js, TypeScript, TailwindCSS, and base layout

---
+++ my-parish/src/app/page.tsx
@@ -0,0 +1,10 @@
+import NewsContainer from "@/containers/NewsContainer";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center">Home</h1>
+      <NewsContainer/>
+    </div>
+  );
+}
---
+++ my-parish/src/containers/NewsContainer.tsx
@@ -0,0 +1,5 @@
+export default function NewsContainer() {
+  return (
+    <div>Aktualności</div>
+  )
+}
